3f5dc1f677d9923f965cc38bf59572e076b1568b
Robert McCallough <robertmccallough@hotmail.co.uk>
2017-06-30 15:06:17 +0100

    initial pseudo code for css

--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 console.log('loaded');
 
 // 1. Create an variable with an array to hold the Random colours. At least six colours; red, green, blue, orange, pink, black.
+
 // 2. Create variables for the Random colours, the Chosen Colours, timer, counter, score and round.
+
 // 3. Create a function that Randomizes the colours and selects three from the array to be shown in the "matching colour" class/section.
+
 // 4. Create a function that only shows the colours for 2 seconds and then fades away.
+
 // 5. Create a function that takes the 3 random colours and displays them in random order in the "colour order" class/section.
+
 // 6. Create a function where the player can click on all 3 of the circles in "matching colour" section. When the player clicks on the circle the colour appears. If the player clicks the incorrect order then all circles return to the colour white. If the player clicks in the correct order then..
+
 // 7. Create a function where if the player wins then all colours used are pushed and a new round starts and the player gets a score of 1.
+
+// 7.a.BONUS -- Create a function which increases the difficulty of the order when the player gets a higher score.
+
 // 8.Create a function where there is a 1 minute timer. When the timer runs out the game ends. Also include the final score.
+
 // 9. Create a function that loads the game and starts the counter when the start button is clicked.
+
 // 10. Create a function that resets the game when it ends.
