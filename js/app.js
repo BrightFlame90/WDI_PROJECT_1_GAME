b8e98eb92dacc880be3c9abcb8761d176bcf74a4
Robert McCallough <robertmccallough@hotmail.co.uk>
2017-07-10 13:29:14 +0100

    html updated/modified

--- js/app.js
+++ js/app.js
@@ -13,3 +13,27 @@ console.log('loaded');
 
 // https://codepen.io/BenLBlood/pen/LGLEoJ
 // https://codeplanet.io/building-simon-says-javascript/
+
+function newRound() {
+  var sequence = [1,2,1]; //red, green, red
+  animate(sequence);
+}
+
+function animate(sequence) {
+  var i = 0;
+  var interval = setInterval(function() {
+    lightUp(sequence[i]);
+
+    i++;
+    if (i >= sequence.length) {
+      clearInterval(interval);
+    }
+  }, 600);
+}
+function lightup(tile) {
+  var $tile = $('[data-tile=' + tile + ']').addClass('lit');
+  window.setTimeout(function() {
+    $tile.removeClass('lit');
+  }, 300);
+
+}
